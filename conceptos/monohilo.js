ea5b4cf6d667e6815ffa73d42f295f2704a6cb08
Omarvillada <olvillada@gmail.com>
2022-09-02 13:59:15 -0500

    Monohilo: implicaciones en diseño y seguridad

---
+++ conceptos/monohilo.js
@@ -0,0 +1,13 @@
+console.log('Hello World');
+
+let i = 0;
+setInterval(function(){ // Funcion Asincrona
+    console.log(i);
+    i++;
+/*     if(i === 5){
+        var a = 3 + z;
+    } */
+    //console.log('I am ready')
+}, 1000)
+
+console.log('second instruction');
